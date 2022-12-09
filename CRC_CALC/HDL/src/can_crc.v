74ee1538d23f9bf7eaae8e6576245f6937bcaff2
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2022-12-09 07:54:05 -0800

    fix output assignments

--- CRC_CALC/HDL/src/can_crc.v
+++ CRC_CALC/HDL/src/can_crc.v
@@ -1,48 +1,49 @@
 `timescale 1ns / 1ps
 
 module can_crc (
-  input clk,
-  input rst_n,
-  input en,
-  input din,
-  output [14:0] crc
+  input clk,          // 100 MHz
+  input rst_n,        // active low reset
+  input en,           // enable activates @ SOF, deactivates after last bit of DATA field in frame is received
+  input din,          // data input from CAN reciever
+  output [14:0] crc,  // CRC 
+  output crc_ready    // indicates CRC has been calculated
 );
   
   reg [14:0] r_crc = 15'h0000;        // init crc reg
-  reg [14:0] r_crc_shift = 15'h0000;  // init crc shift reg
+  reg r_crc_ready = 1'b0;             // init flag register
   
-  reg r_crcnxt = 1'b0;
+  wire w_crcnxt = din ^ r_crc[14];    			  // XOR of two bits (see pg. 13 of Bosch pdf)
+  wire [14:0] w_crc_shift = {r_crc[13:0], 1'b0};  // init left shift
   
   always @ (posedge clk or negedge rst_n)
     begin
       if (!rst_n)
         begin 
           r_crc <= 15'h0000;
-          r_crcnxt <= 1'b0;
+          r_crc_ready <= 1'b0;          
         end
       else
-        begin
-          if (en)
-            begin
-              r_crcnxt <= r_crc[14] ^ din;
-              r_crc_shift <= {r_crc[13:0], 1'b0};
-                if (r_crcnxt)
-                  begin 
-                    r_crc <= r_crc_shift ^ 15'h4599;
-                  end 
-                else 
-                  begin 
-                    r_crc <= r_crc_shift;
-                  end 
-            end 
+        begin 
+          if (en) 
+            begin 
+              if (w_crcnxt)
+                begin 
+                  r_crc <= w_crc_shift ^ 15'h4599;
+                end 
+              else 
+                begin 
+                  r_crc <= w_crc_shift;
+                end
+            end
           else 
             begin 
-              r_crc <= 15'h0000;
-            end 
-        end
-      end 
+              r_crc <= w_crc_shift;
+            end
+        end 
+    end  
   
-  //assign crc <= r_crc;
+  assign crc = r_crc;
+  assign crc_ready = r_crc_ready;
   
 endmodule 
 
