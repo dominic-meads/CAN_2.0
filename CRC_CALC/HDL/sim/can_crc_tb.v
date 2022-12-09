8a2a07ad91d28b402160f07af3aff23ddc8da96b
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2022-12-09 10:18:56 -0800

    fixed timing issues and update uut ports

--- CRC_CALC/HDL/sim/can_crc_tb.v
+++ CRC_CALC/HDL/sim/can_crc_tb.v
@@ -6,15 +6,18 @@ module tb();
   reg en;
   reg din;
   wire [14:0] crc;
+  wire crc_ready;
   
-  always #5 clk = ~clk;  // 100 MHz
+  always #500 clk = ~clk;  // 1 MHz
   
-  can_crc uut(clk, rst_n, en, din, crc);
+  can_crc #(1, 250) uut (clk, rst_n, en, din, crc, crc_ready);
   
   reg [9:0] r_idle = 10'hfff;  // IDLE time at end of frame
   reg [82:0] r_can_rx = 83'b00001100000100010000100000000000001000010000111010100000000000000000000000000000000;  // add extra one to see where data ends
   reg [92:0] r_bitstream;
-  integer i;
+  
+  integer i;  // for loop to ship out bitstream
+  integer last_time = 4000;  // time for last bit in bitstream
   
   initial
     begin 
@@ -26,15 +29,10 @@ module tb();
       din = 0;
       r_bitstream = {r_idle, r_can_rx};  // put idle time in front
       #10
-      rst_n = 1;
+      rst_n = 1;  // release rst
       #20
-      en = 1;
-      for(i = 92; i >= 0; i=i-1)
+      for(i = 92; i >= 0; i=i-1)  // start shipping out bitstream
         begin 
           din = r_bitstream[i];
-          #2000;  // 500 Kbit/s
-        end 
-      #200000
-      $finish;
-    end 
-endmodule
+          if (i <= 82 && i != 0)  // after idle bits from previous frame, SOF starts, drive "en" high 
+         
