100424bf96b609fd647209060a24864bda808f56
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2023-01-30 08:32:36 -0800

    clean up, comment, as description

--- CRC_CALC/HDL/src/crc_calc.v
+++ CRC_CALC/HDL/src/crc_calc.v
@@ -20,9 +20,6 @@
 // Revision 0.01 - File Created
 // Additional Comments:                    
 //////////////////////////////////////////////////////////////////////////////////
-
-
-
 `timescale 1 ns / 1 ps
 
 module crc_calc(
