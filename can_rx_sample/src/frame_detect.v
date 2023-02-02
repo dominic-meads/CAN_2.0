fef77eaf9806245f799755cd1adf6de2cdc366c0
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2023-02-02 11:37:40 -0800

    finish up counter, add edge detection on can bus

--- can_rx_sample/src/frame_detect.v
+++ can_rx_sample/src/frame_detect.v
@@ -37,23 +37,25 @@ module frame_detect#(
   parameter clk_speed_MHz = 100,
   parameter can_bit_rate_KHz = 1000
 )(
-  input clk,     // 100 MHz system clk
+  input clk,         // 100 MHz system clk
   input rst_n,
-  input can_rx,  // data in
-  output sof     // start of frame, if high, enables the data_sample module 
+  input can_rx,      // data in
+  output sof_detect  // flag that detects start of frame, if high, enables the data_sample module 
 ); 
   
   // counter register to keep track of clk cycles per 11 recessive bits sent on the can line
   reg [$clog2(((clk_speed_MHz * 1000) / can_bit_rate_Kbits)*11)-1:0] r_frame_end_time = 0;
   
-  reg r_sof = 1'b0;
+  // edge detection registers
+  reg r_sof_detect = 1'b0;
+  reg r_sof_temp = 1'b0;
+  
   
   // up counter timer
   always @ (posedge clk or negedge rst_n)
     begin 
       if (!rst_n)
         begin 
-          r_sof <= 1'b0; 
           r_frame_end_time <= 0;
         end  // if (!rst_n)
       else 
@@ -68,11 +70,59 @@ module frame_detect#(
                 begin 
                   r_frame_end_time <= 0;  // reset counter
                 end  // else
-            end
-          
-          
-          add always block to start sof when counter is maxed. 
-          add in the else statement for when can_rx is not high. 
+            end  // if (can_rx)
+          else 
+            begin
+              r_frame_end_time <= 0;
+            end  // else 
+        end  // else 
+    end  // always
+  
+  
+  // edge detect register delay
+  always @ (posedge clk or negedge rst_n)
+    begin 
+      if (!rst_n)
+        begin 
+          r_sof_temp <= 1'b0;
+        end  // if (!rst_n)
+      else 
+        begin
+          r_sof_temp <= can_rx;
+        end  // else
+    end  // always 
+      
+
+  // Detect SOF after 11 recessive bits
+  always @ (posedge clk or negedge rst_n)
+    begin 
+      if (!rst_n)
+        begin 
+          r_sof_detect <= 1'b0;
+        end  // if (!rst_n)
+      else 
+        begin 
+          // After the counter counts for 10 consective recessive bits, count halfway through the 11th recessive bit, then look for the falling edge on the bus
+          //                         |-------- time for 10 recessive bits -----------|   |----- half of another recessive bit period ----|
+          if (r_frame_end_time >= (  (((clk_speed_MHz * 1000) / can_bit_rate_Kbits)*10) + ((clk_speed_MHz * 1000) / (2*can_bit_rate_Kbits))  )) 
+            begin
+              if (!can_rx && r_sof_temp)  // can_rx is now low, but previous state was high, transition to dominant has occurred, this is the start of frame.
+                begin
+                  r_sof_detect <= 1'b1;
+                end  // if (!can_rx...
+              else 
+                begin 
+                  r_sof_detect <= 1'b0;
+                end  // else 
+            end  // if (r_frame_end_time...
+          else 
+            begin 
+              r_sof_detect <= 1'b0;
+            end  // else 
+        end  // else 
+    end  // always
+  
+    
               
           
   
