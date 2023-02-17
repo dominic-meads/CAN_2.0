1123ec154c508ae407c3f656dfe6b1e173774723
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2023-02-17 14:28:31 -0800

    start state machine for data sample

--- can_rx_sample/src/data_sample.v
+++ can_rx_sample/src/data_sample.v
@@ -8,7 +8,8 @@
 // Project Name: CAN_2.0
 // Target Devices: 
 // Tool Versions: 
-// Description: Samples the incoming bitstream on the CAN lines.
+// Description: Samples the incoming bitstream on the CAN lines. Sampling is enabled
+//              by the SOF flag.
 //                    
 // Dependencies: 100 MHz clock, 1 Mb/s data rate
 // 
@@ -22,15 +23,45 @@ module can_rx_sample
   #(parameter clk_speed_MHz = 100,        // system clock on FPGA board
     parameter can_bit_rate_Kbits = 1000)  // CAN speed on my vehicle
   (
-  input clk,    // 100 MHz
-  input rst_n,  // active low
-  input en,     // active at SOF
-  input din,    // rx in
-  output dout   // registered data out to other modules
+  input clk,     // 100 MHz
+  input rst_n,   // active low
+  input en,      // active at SOF
+  input din,     // rx in
+  output dout,   // registered data out to other modules
+  output dvalid  // data valid  
   );
   
+  // states
+  parameter IDLE = 1'b0;
+  parameter SAMPLE = 1'b1;
+  
+  // state registers
+  reg r_present_state;
+  reg r_next_state; 
+  
+  // counter and output registers
   reg [$clog2((clk_speed_MHz * 1000) / can_bit_rate_Kbits)-1:0] r_clks_per_bit_counter = 0;
   reg r_dout = 1'b0;
+  reg r_dvaild = 1'b0;
+  
+  // present state logic
+  always @ (posedge clk or negedge rst_n)
+    begin 
+      if (!rst_n)
+        begin 
+          r_present_state <= IDLE;
+        end  // if (!rst_n)
+      else 
+        begin
+          r_present_state <= r_next_state;
+        end  // else
+    end  // always 
+  
+  // next state logic
+  always @ (r_present_state, en)
+    begin
+      r_next_state = IDLE; 
+      case (r_present_state);
       
   // implement up counter
   always @ (posedge clk or negedge rst_n)
@@ -65,16 +96,19 @@ module can_rx_sample
       if(!rst_n)
         begin 
           r_dout <= 1'b0;
+          r_dvalid <= 1'b0;
         end  // (!rst_n)
       else 
         begin 
           if (r_clks_per_bit_counter == ((clk_speed_MHz * 1000) / can_bit_rate_Kbits)/2 - 1) 
             begin
               r_dout <= din;  // sample data at halfway point during bit period
+              r_dvalid <= 1'b1;
             end  // if (r_clk_per...
         end  // else
     end  // always
       
   assign dout = r_dout;
+  assign 
       
 endmodule  // data_sample
