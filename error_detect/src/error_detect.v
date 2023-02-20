1da0febe16483f3f65c54636171e566252d4a973
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2023-02-20 11:42:26 -0800

    Create error_detect.v

---
+++ error_detect/src/error_detect.v
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Dominic Meads
+// 
+// Create Date: 02/20/2023 
+// Design Name: 
+// Module Name: error_detect
+// Project Name: CAN_2.0
+// Target Devices: 
+// Tool Versions: 
+// Description: Samples the incoming bitstream on the CAN lines. Sampling is enabled
+//              by the SOF flag, and continued until frame has reached the CRC field.
+//                    
+// Dependencies: 100 MHz clock, 1 Mb/s data rate
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:                    
+//////////////////////////////////////////////////////////////////////////////////
